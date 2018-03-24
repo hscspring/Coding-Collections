43522463d9c1781c59f9bb3566a40e644ab7a28a
Yam <haoshaochun@gmail.com>
2018-03-24 18:20:46 +0800

    update

---
+++ jstraining-master/yam/fsm.js
@@ -0,0 +1,16 @@
+var StateMachine = require('javascript-state-machine');
+var fsm = new StateMachine({
+    init: 'solid',
+    transitions: [
+      { name: 'melt',     from: 'solid',  to: 'liquid' },
+      { name: 'freeze',   from: 'liquid', to: 'solid'  },
+      { name: 'vaporize', from: 'liquid', to: 'gas'    },
+      { name: 'condense', from: 'gas',    to: 'liquid' }
+    ],
+    methods: {
+      onMelt:     function() { console.log('I melted')    },
+      onFreeze:   function() { console.log('I froze')     },
+      onVaporize: function() { console.log('I vaporized') },
+      onCondense: function() { console.log('I condensed') }
+    }
+  });
