0fc371f080644d44b58132b696455c479cddfe29
Yam <haoshaochun@gmail.com>
2019-07-13 21:01:06 +0800

    update leetcode

--- Exercise/LeetCode/3Longest-Substring-Without-Repeating-Characters☆☆☆☆/Longest-Substring-Without-Repeating-Characters.md
+++ Exercise/LeetCode/3Longest-Substring-Without-Repeating-Characters☆☆☆☆/Longest-Substring-Without-Repeating-Characters.md
@@ -25,3 +25,4 @@ Explanation: The answer is "wke", with the length of 3.
              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 ```
 
+A sliding window is an abstract concept commonly used in array/string problems. A window is a range of elements in the array/string which usually defined by the start and end indices, i.e. [i, j)[*i*,*j*) (left-closed, right-open). A sliding window is a window "slides" its two boundaries to the certain direction.
--- Exercise/LeetCode/7Reverse-Integer/Reverse-Integer.py
+++ Exercise/LeetCode/7Reverse-Integer/Reverse-Integer.py
@@ -7,21 +7,21 @@ class Solution:
             m = int(a%(10**i)/(10**(i-1)))
             num += m*(10**(length-i))
         return -num if x < 0 else num
-    def get_revsersed_num2(self, x):
-        s = str(x)
-        if x < 0:
-            s = s[1:]
-            res = "-"
-        else:
-            res = ""
-        for i in range(len(s)-1, -1, -1):
-            res += s[i]
-        num = int(res)
-        return num
     def reverse(self, x: int) -> int:
         num = self.get_revsersed_num(x)
         if num > (2**31) -1 or num < -(2**31):
             num = 0
         return num
         
-        
+
+def get_revsersed_num2(self, x):
+    s = str(x)
+    if x < 0:
+        s = s[1:]
+        res = "-"
+    else:
+        res = ""
+    for i in range(len(s)-1, -1, -1):
+        res += s[i]
+    num = int(res)
+    return num
