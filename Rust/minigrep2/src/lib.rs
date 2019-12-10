ff743ae53212b5785fa4727b5c66f5585ab5dd13
Yam <haoshaochun@gmail.com>
2019-12-10 12:04:04 +0800

    add add

--- Rust/minigrep2/src/lib.rs
+++ Rust/minigrep2/src/lib.rs
@@ -1,6 +1,26 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
 use std::error::Error;
 use std::fs;
 use std::env;
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = minigrep2::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
 
 pub struct Config {
     pub query: String,
