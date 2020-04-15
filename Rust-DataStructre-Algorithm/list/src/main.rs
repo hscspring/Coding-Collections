a7ab30c49b508dfa8d748ba0362ed0ac1a22c3dd
Yam <haoshaochun@gmail.com>
2020-04-15 18:01:22 +0800

    update Rust

--- Rust-DataStructre-Algorithm/list/src/main.rs
+++
@@ -1,7 +0,0 @@
-extern crate list;
-
-use list::singly_linked_list;
-
-fn main() {
-    println!("Hello, world!");
-}
--- Rust/add/adder/src/main.rs
+++
@@ -1,8 +0,0 @@
-use rand;
-use add_one;
-
-fn main() {
-    let num = 10;
-    println!("hello, world! {} plus one is {}!", num, add_one::add_one(num));
-    println!("Hello, world!");
-}
